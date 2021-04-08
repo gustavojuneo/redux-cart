026146c6e5da234f47c6cd8dae1319175ee3f54a
Gustavo Juneo <gustavojuneo@outlook.com>
2021-04-07 21:32:11 -0300

    Initializing with Redux

---
+++ src/components/Catalog.tsx
@@ -0,0 +1,9 @@
+import { useSelector } from 'react-redux'
+
+const Catalog: React.FC = () => {
+  const catalog = useSelector(state => state)
+
+  return <h1>Catalog</h1>
+}
+
+export default Catalog
