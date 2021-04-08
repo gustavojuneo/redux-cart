f2b997fca43023754f81d25ed7bf4e07d20d0de8
Gustavo Juneo <gustavojuneo@outlook.com>
2021-04-08 01:10:47 -0300

    use redux actions and reducer

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,9 +1,28 @@
-import { useSelector } from 'react-redux'
+import { useEffect, useState } from 'react'
+
+import api from '../services/api'
+
+import CatalogItem from './CatalogItem'
+import { IProduct } from '../store/modules/cart/types'
 
 const Catalog: React.FC = () => {
-  const catalog = useSelector(state => state)
+  const [catalog, setCatalog] = useState<IProduct[]>([])
 
-  return <h1>Catalog</h1>
+  useEffect(() => {
+    api.get('products').then(response => {
+      setCatalog(response.data)
+    })
+  }, [])
+
+  return (
+    <main>
+      <h1>Catalog</h1>
+
+      {catalog.map(product => (
+        <CatalogItem key={product.id} product={product} />
+      ))}
+    </main>
+  )
 }
 
 export default Catalog
